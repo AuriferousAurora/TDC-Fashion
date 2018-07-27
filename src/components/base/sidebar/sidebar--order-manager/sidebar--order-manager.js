c767ea91b3c7fee5f29f1c85241e134178662ede
Kyle Everett Lang <kyle.everett.lang@gmail.com>
2018-07-26 22:28:09 -0500

    Added icons to sidebar.

--- src/components/base/sidebar/sidebar--order-manager/sidebar--order-manager.js
+++ src/components/base/sidebar/sidebar--order-manager/sidebar--order-manager.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import Sidebar from '../sidebar';
 import Logo from '../../../logo/logo.js';
+import DashboardIcon from './icons/IconDashboardEnable.png';
+import NewOrderIcon from './icons/IconNewOrder.png';
+import PaymentIcon from './icons/IconPaymentsDisable.png';
+import ShippingIcon from './icons/IconShippingDisable.png';
+import NotificationIcon from './icons/IconNotificationsDisable.png';
 
 class SidebarOrderManager extends Sidebar {
   render() {
     return (
       <div className='sidebar sidebar--order-manager'>
         <Logo/>
+        <div className='sidebar__icon sidebar__icon--dashboard'><img src={DashboardIcon} alt='dashboard'/></div>
+        <div className='sidebar__icon sidebar__icon--new-order'><img src={NewOrderIcon} alt='new-order'/></div>   
+        <div className='sidebar__icon sidebar__icon--payments'><img src={PaymentIcon} alt='payment'/></div>
+        <div className='sidebar__icon sidebar__icon--shipping'><img src={ShippingIcon} alt='shipping'/></div>
+        <div className='sidebar__icon sidebar__icon--notifications'><img src={NotificationIcon} alt='notifications'/></div>
       </div>
     );
   }
