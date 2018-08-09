dc4954088f6c1decb777eb73d8cb42ebb2c6d105
Kyle Everett Lang <kyle.everett.lang@gmail.com>
2018-08-09 17:50:13 -0500

    Replaced menu button on landing page.

--- src/components/landing/header/header.js
+++ src/components/landing/header/header.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Header } from '../../base/header/header';
 import DarkLogo from './assets/dark-logo.png';
+import Burger from './assets/burger.png';
 
 export const LandingHeader = (props) => (
-  <Header id='landing-sidebar-toggle' className={'header--landing landing__header'} left={<img src={DarkLogo} alt='TDC Fashion'/>} right={<button onClick={props.onClick}>Menu</button>}/>
+  <Header id='landing-sidebar-toggle' className={'header--landing landing__header'} left={<img src={DarkLogo} alt='TDC Fashion'/>} right={<img className='menu-button' src={Burger} alt='Menu' onClick={props.onClick}/>}/>
 );
