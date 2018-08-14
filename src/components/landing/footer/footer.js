9b064298d02a57ca3c357adeeff7179a06a5322c
Kyle Everett Lang <kyle.everett.lang@gmail.com>
2018-08-14 17:21:37 -0500

    Completed footer wireframe.

--- src/components/landing/footer/footer.js
+++ src/components/landing/footer/footer.js
@@ -2,35 +2,25 @@ import React, { Fragment } from 'react';
 import { Footer } from '../../base/footer/footer';
 import { Link } from 'react-router-dom'
 
+import FacebookIcon from './assets/facebook-primary.png';
+import InstagramIcon from './assets/instagram-primary.png';
+import TwitterIcon from './assets/twitter-primary.png';
+
 export const LandingFooter = (props) => (
   <Footer className={'footer--landing landing__footer'}
-    left={
+    links={
     <Fragment>
-      <Link className='link footer__link' to='/'><span>Discover Our Story</span></Link>
-      <Link className='link footer__link' to='/'><span>Discover Our Story</span></Link>
-      <Link className='link footer__link' to='/'><span>Discover Our Story</span></Link>
-      <Link className='link footer__link' to='/'><span>Discover Our Story</span></Link>
-      <Link className='link footer__link' to='/'><span>Discover Our Story</span></Link>
+      <Link className='link footer__link' to='/'><span>Facilities</span></Link>
+      <Link className='link footer__link' to='/'><span>Suppliers</span></Link>
+      <Link className='link footer__link' to='/'><span>Success Stories</span></Link>
+      <Link className='link footer__link' to='/'><span>Newsletter</span></Link>
+      <Link className='link footer__link' to='/'><span>Blog</span></Link>
     </Fragment>}
-    right={
+    social={
     <Fragment>
-      <Link className='link footer__link' to='/'><span>Discover Our Story</span></Link>
-      <Link className='link footer__link' to='/'><span>Discover Our Story</span></Link>
-      <Link className='link footer__link' to='/'><span>Discover Our Story</span></Link>
-      <Link className='link footer__link' to='/'><span>Discover Our Story</span></Link>
-      <Link className='link footer__link' to='/'><span>Discover Our Story</span></Link>
-    </Fragment>}
-    bottom={
-      <Fragment>
-        <div className='bottom__left'>
-          <Link className='link bottom__link link--spacer' to='/'><span>Contact Us</span></Link>
-          <Link className='link bottom__link' to='/'><span>Contact Us</span></Link>
-          <Link className='link bottom__link' to='/'><span>Contact Us</span></Link>
-          <Link className='link bottom__link' to='/'><span>Contact Us</span></Link>
-          <Link className='link bottom__link' to='/'><span>Contact Us</span></Link>
-        </div>
-        <div className='bottom__right'>
-          <svg></svg>
-        </div>
-      </Fragment>}/>
+      <img id='landing__facebook-link' className='landing__social-link' src={FacebookIcon} alt='Facebook'/>
+      <img id='landing__instagram-link' className='landing__social-link' src={InstagramIcon} alt='Instagram'/>
+      <img id='landing__twitter-link' className='landing__social-link' src={TwitterIcon} alt='Twitter'/>
+    </Fragment>}>
+  </Footer>
 );
--- src/components/landing/values/values.js
+++ src/components/landing/values/values.js
@@ -9,18 +9,18 @@ export const LandingValues = (props) => (
     <div id='value__transparent' className='values__title-bar'>
       <span className='value__descriptor'>Transparent</span>
     </div>   
-    <div className='values__description transparent__description'></div>
+    {/* <div className='values__description transparent__description'></div> */}
     <div id='value__speed' className='values__title-bar'>
       <span className='value__descriptor'>Speed</span>
     </div>
-    <div className='values__description transparent__description'></div>
+    {/* <div className='values__description transparent__description'></div> */}
     <div id='value__ethical' className='values__title-bar'>
       <span className='value__descriptor'>Ethical</span>
     </div>
-    <div className='values__description transparent__description'></div>   
+    {/* <div className='values__description transparent__description'></div>    */}
     <div id='values__economic' className='values__title-bar'>
       <span className='value__descriptor'>Economic</span>
     </div>
-    <div className='values__description transparent__description'></div>   
+    {/* <div className='values__description transparent__description'></div>    */}
   </Content>
 );
